a9d44e7b260bc8432d332019da26c35463f0e705
aisha8056 <113588435+aisha8056@users.noreply.github.com>
2023-02-16 11:50:49 +0100

    test

--- Toets/Untitled-1.py
+++
@@ -1,2 +0,0 @@
-iphone_prijs = round(float(input("Hoe duur is de Iphone? ")), 2)
-samsung_prijs = round(float(input("Hoe duur is de Samsung? ")), 2)
--- Toets/smartphone.pyn.py
+++
@@ -1,9 +0,0 @@
-#input
-
-iphone_prijs = 800
-samsung_prijs =  750
-
-
-iphone_prijs = round(float(input("Hoe duur is de iPhone?")), 2)
-samsung_prijs = round(float(input("Hoe duur is de Samsung?")), 2)
-print('Welke telefoon is het duurst?')
